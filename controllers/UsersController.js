f3ecaccd084245f13d2489125e5f1cdb4d3a7d36
747-diego <725@holbertonschool.com>
2021-06-17 17:48:07 -0400

    [ESLINT ERROR]

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,13 +3,10 @@ import dbClient from '../utils/db';
 
 export const postNew = async (req, res) => {
   const { email, password } = req.body;
-
+  let user = await dbClient.findUser({ email });
   if (!email)res.status(400).json({ error: 'Missing email' });
   if (!password)res.status(400).json({ error: 'Missing password' });
-
-  let user = await dbClient.findUser({ email });
   if (user)res.status(400).json({ error: 'Already exist' });
-
   user = await dbClient.createUser(email, sha1(password));
   res.status(201).json(user);
 };
