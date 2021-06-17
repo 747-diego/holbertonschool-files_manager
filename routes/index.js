76cfeb0175263278c8210c4fb6b293de1accd70b
747-diego <725@holbertonschool.com>
2021-06-17 17:31:43 -0400

    [CREATING NEW USER]

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { getStatus, getStats } from '../controllers/AppController';
+import { postNew } from '../controllers/UsersController';
 
+const post = postNew;
 const status = getStatus;
 const stats = getStats;
 const router = express.Router();
 router.get('/status', status);
 router.get('/stats', stats);
+router.post('/users', post);
 
 export default router;
