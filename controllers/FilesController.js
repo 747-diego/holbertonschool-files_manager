d9d001cd87e11cb20ff7e3f465d2d29d75b2bddb
747-diego <725@holbertonschool.com>
2021-06-18 09:59:19 -0400

    [TEST]

---
+++ controllers/FilesController.js
@@ -0,0 +1,6 @@
+import dbClient from '../utils/db';
+import fileQueue from '../utils/image_upload';
+import { writeFile } from '../utils/files';
+import { ObjectID } from 'mongodb';
+import { v4 as uuid } from 'uuid';
+
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import auth from '../utils/auth';
 import { getStatus, getStats } from '../controllers/AppController';
 import { postNew, getMe } from '../controllers/UsersController';
 import { getConnect, getDisconnect } from '../controllers/AuthController';
+import { postUpload } from '../controllers/FilesController';
 
 const router = express.Router();
 const status = getStatus;
@@ -11,6 +12,7 @@ const post = postNew;
 const connected = getConnect;
 const disconnected = getDisconnect;
 const user = getMe;
+const upload = postUpload;
 
 router.get('/status', status);
 router.get('/stats', stats);
@@ -18,5 +20,5 @@ router.post('/users', post);
 router.get('/connect', connected);
 router.get('/disconnect', auth, disconnected);
 router.get('/users/me', auth, user);
-
+router.post('/files', auth, upload);
 export default router;
